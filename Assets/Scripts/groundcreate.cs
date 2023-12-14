985eb012298f51ba97ec189a106a835932ac671c
katumatu <93749855+katumatu@users.noreply.github.com>
2023-12-14 09:03:38 +0900

    難易度調整
    地形ブロックのサイズ変更
    地形ブロックの生成位置変更

--- Assets/Scripts/groundcreate.cs
+++ Assets/Scripts/groundcreate.cs
@@ -16,6 +16,6 @@ public class groundcreate : MonoBehaviour
     void GenRock()
     {
         //画面の上部端より少し上から、画面の左端から右端の間でランダムな位置に隕石を生成する
-        Instantiate(groundPrefab, new Vector3(14.0f, -5.0f + 5.0f * Random.value, 0), Quaternion.identity);
+        Instantiate(groundPrefab, new Vector3(12.0f + 2.0f * Random.value, -5.0f + 5.0f * Random.value, 0), Quaternion.identity);
     }
 }

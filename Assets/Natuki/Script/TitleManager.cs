206fbf66cba24b24982e8ecc3216d91a148d0007
Natsuki-Nagatsa <natumikan.aa5@gmail.com>
2023-12-14 09:50:01 +0900

    タイトル回収00
    スタート内にある文字の点滅処理の調整
    スタートボタンを押すゲームをプレイできるようにしました。

--- Assets/Natuki/Script/TitleManager.cs
+++ Assets/Natuki/Script/TitleManager.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.UI; //UI関係の為のライブラリ
+using UnityEngine.SceneManagement; //ロード関係上必要なライブラリ
 
 /// <summary>
 /// system構造体変数ひとまとめしました。
@@ -12,6 +13,7 @@ public struct SystemDataSet
     public GameObject startButton; //開始ボタン
     public GameObject exitButton; //終了ボタン
     public Text startText; //開始ボタン内にあるテキスト
+    public string LodedName;
 
 }
 
@@ -19,7 +21,7 @@ public class TitleManager : MonoBehaviour
 {
     //点滅処理処理関連変数
     float time;
-    float speed = 0.5f;
+    float speed = 0.3f;
 
     public SystemDataSet system; //実体宣言
 
@@ -31,8 +33,7 @@ public class TitleManager : MonoBehaviour
 
     public void StartBUtton()
     {
-        system.startButton.SetActive(false);
-        system.exitButton.SetActive(false);
+        SceneManager.LoadScene(system.LodedName);
     }
 
     /// <summary>
@@ -42,7 +43,7 @@ public class TitleManager : MonoBehaviour
     /// <returns></returns>
     Color Text_Invicivil(Color color)
     {
-        time += Time.deltaTime * 5.0f * speed; //点滅する速度設定
+        time += Time.deltaTime * 4.0f * speed; //点滅する速度設定
         color.a = Mathf.Sin(time); //α値を変更して点滅処理を促す
 
 
